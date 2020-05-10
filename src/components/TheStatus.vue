a1a4390f7af77d2f2b6f7a6124670bafb8a9e055
shinnosuke-komiya <shinnosuke828@gmail.com>
2020-05-10 15:24:16 +0900

    feat: import vuex actions

---
+++ src/components/TheStatus.vue
@@ -0,0 +1,63 @@
+<template>
+  <section>
+    <div class='status-list'>
+      <ul>
+        <li>{{levelList[statusLevel].job}}</li>
+        <li>レベル：{{statusLevel}}</li>
+        <li>ＨＰ：{{levelList[statusLevel].hp}}</li>
+        <li>ＭＰ：{{levelList[statusLevel].mp}}</li>
+        <li>ＥＸ：{{stateEx}}</li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      levelList: [
+        {
+          job: 'begginer',
+          hp: 1,
+          mp: 1,
+          ex: 1
+        },
+        {
+          job: 'friend',
+          hp: 100,
+          mp: 100,
+          ex: 2
+        }
+      ]
+    }
+  },
+  computed: {
+    stateEx: function(){
+      return this.$store.state.ex
+    },
+    statusLevel: function(){
+      let tmpLevel = 0
+      for (let i = 0; this.levelList.length > i; i++){
+        if (this.stateEx > this.levelList[i].ex){
+          tmpLevel = i
+        }
+      }
+      return tmpLevel
+    }
+  }
+}
+</script>
+
+<style>
+
+.status-list{
+  text-align: center;
+  width: 130px;
+  height: 120px;
+  padding:10px;
+  margin-bottom:10px;
+  border: 2px solid #fff;
+  border-radius: 10px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import TheStatus from '@/components/TheStatus'
+import TheRoom from '@/components/TheRoom'
+import TheCommand from '@/components/TheCommand'
+
 
 Vue.use(Router)
 
@@ -8,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'TheCommand',
+      component: TheCommand
     }
   ]
 })
